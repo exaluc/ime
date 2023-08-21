c9ae153a2829216ccca49e74eef3d176dcaefa4e
Lucian <42606+exaluc@users.noreply.github.com>
2023-08-21 02:21:49 +0200

    extractor

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Image Metadata Generator',
+  title: 'Image Metadata Extractor',
   description: "Easily extract and view the embedded metadata from your images. Whether it's EXIF, IPTC, or XMP; get detailed insights in seconds.",
 }
 
